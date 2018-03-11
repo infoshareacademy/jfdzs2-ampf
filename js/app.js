ffd5f5d5b36e4fb2f949d04d73c4ae6fad466722
Filip <filip.burakowski@gmail.com>
2018-03-11 20:23:50 +0100

    Adding basic js file for game project.

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// - PODSTAWOWE INFO -
+
+
+// - OBIEKTY -
+
+
+// - STARTOWANIE GRY -
+
+
+// - WYŚWIETLANIE INSTRUKCJI -
+
+
+// - WYŚWIETLANIE - WYNIKOW -
+
+
+// - RANDOMIZOWANIE OWOCÓW -
+
+
+// - POZYCJONOWANIE OWOCÓW -
+
+
+// - SPADANIE OWOCÓW -
+
+
+// - RUCH LUDZIKA -
+
+
+// - KOLIZJE -
+
+
+// - KONIEC GRY -
+
+
+// - LICZENIE PUNKTÓW -
+
+
+// - WYŚWIETLANIE PUNKTÓW -
+
+
+// - MUZYKA -
+
+
+// - EFEKTY MUZYCZNE (BEKI I INNE) -
+
+
+// - PAUZA -
+
+
+// - WZNOWIENIE -
+
+
+// - RESTART -
+
+
+
+
+
