8a7cae7d18152b4b50c7bc6db7842f00df128061
MacS <soltykiewiczm@gmail.com>
2018-03-25 20:56:54 +0200

    added cookie functions set and get cookie

--- js/app.js
+++ js/app.js
@@ -7,4 +7,24 @@ $(".cookie-button").click(function () {
     $(".cookie-message").fadeOut("slow");
 })
 
+function setCookie(cookie-name, cookie-value, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cookie-name + "=" + cookievalue + ";" + expires + ";path=/";
+}
 
+function getCookie(cname) {
+    var name = cookie - name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
